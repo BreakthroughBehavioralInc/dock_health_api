387ad6bd5655c8e43e463dad6e812577d0e8a206
Au Lee <c7k3v9@sualee-m1.mdlive.local>
2022-06-07 13:48:44 -0400

    Fix typo

--- lib/dock_health_api/crud/delete.rb
+++ lib/dock_health_api/crud/delete.rb
@@ -2,7 +2,7 @@ module DockHealthApi
   module Crud
     module Delete
       def delete(id)
-        response = execute_request(:delete, "#{resource_url}/#{{id}}")
+        response = execute_request(:delete, "#{resource_url}/#{id}")
         return response.parsed if response.error
         new(response.parsed)
       end
