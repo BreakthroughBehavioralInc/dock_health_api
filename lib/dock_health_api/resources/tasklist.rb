24039b7a49f317ab2c2287d45aba77b6e57e9a07
Au Lee <c7k3v9@sualee-m1.mdlive.local>
2022-06-07 17:25:46 -0400

    Update tasklist, task and webhook

--- lib/dock_health_api/resources/tasklist.rb
+++ lib/dock_health_api/resources/tasklist.rb
@@ -1,9 +1,14 @@
 module DockHealthApi
-  class Task < Resource
+  class TaskList < Resource
     extend DockHealthApi::Crud::Create
-#    extend DockHealthApi::Crud::Delete  ## Waiting for delete to be ad in crud
+    extend DockHealthApi::Crud::Delete
     extend DockHealthApi::Crud::Get
     extend DockHealthApi::Crud::Update
     extend DockHealthApi::Crud::List
+
+    def self.resource_url
+      "#{client.config.resource_url}/api/#{url_version}/list"
+    end
+
   end
 end
--- lib/dock_health_api/resources/webhook.rb
+++ lib/dock_health_api/resources/webhook.rb
@@ -4,5 +4,11 @@ module DockHealthApi
 #    extend DockHealthApi::Crud::Delete  ## Waiting for delete to be ad in crud
     extend DockHealthApi::Crud::Get
     extend DockHealthApi::Crud::Update
+    extend DockHealthApi::Crud::List
+
+    def self.resource_url
+      "#{client.config.resource_url}/api/#{url_version}/developer/#{class_name.downcase}"
+    end
+    
   end
 end
