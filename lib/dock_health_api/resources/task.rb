f1dd48d5754fcb1ee01dd88fbc178b9516966976
Leo Lee <alee@mdlive.com>
2022-06-08 10:29:47 -0400

    Add custom 'list' to Task

--- lib/dock_health_api/resources/task.rb
+++ lib/dock_health_api/resources/task.rb
@@ -5,5 +5,12 @@ module DockHealthApi
     extend DockHealthApi::Crud::Get
     extend DockHealthApi::Crud::Update
     extend DockHealthApi::Crud::List
+
+    def self.list(**params)
+      headers = {"x-api-key": "#{ENV["DOCK_HEALTH_API"]}", "x-user-id": "#{ENV["DOCK_USER"]}", "x-organization-id": "#{ENV["DOCK_ORG"]}"}
+      response = execute_request(:get, "#{resource_url}", headers: headers, body_params: params)
+      return response.parsed
+      new(response.parsed)
+    end
   end
 end
