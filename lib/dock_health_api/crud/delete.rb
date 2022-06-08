04ad4a237532e0e86bdf571a80e2addd634cb6bb
Leo Lee <alee@mdlive.com>
2022-06-08 15:35:11 -0400

    Fix delete and update for TaskList::User

--- lib/dock_health_api/crud/delete.rb
+++ lib/dock_health_api/crud/delete.rb
@@ -2,9 +2,10 @@ require "dotenv/load"
 module DockHealthApi
   module Crud
     module Delete
-      def delete(id)
-        headers = {"x-api-key": "#{ENV["DOCK_HEALTH_API"]}", "x-user-id": "#{ENV["DOCK_USER"]}", "x-organization-id": "#{ENV["DOCK_ORG"]}"}
-        response = execute_request(:delete, "#{resource_url}/#{id}", headers: headers )
+      def delete(**params)
+        id = params.delete(:id)
+        headers = {"Content-Type": "application/json", "x-api-key": "#{ENV["DOCK_HEALTH_API"]}", "x-user-id": "#{ENV["DOCK_USER"]}", "x-organization-id": "#{ENV["DOCK_ORG"]}"}
+        response = execute_request(:delete, "#{resource_url}/#{id}", headers: headers, body_params: params)
         return response.parsed
         new(response.parsed)
       end
--- lib/dock_health_api/resources/tasklist.rb
+++ lib/dock_health_api/resources/tasklist.rb
@@ -11,15 +11,12 @@ module DockHealthApi
     end
 
     class User < TaskList
-      extend DockHealthApi::Crud::Create
-      extend DockHealthApi::Crud::Delete
-      extend DockHealthApi::Crud::Update
+      extend DockHealthApi::Crud::Put
 
       def self.resource_url
         "#{client.config.resource_url}/api/#{url_version}/list/user"
       end
 
-      end
     end
   end
 end
