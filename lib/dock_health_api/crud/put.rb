7069ea206759d95a73aa056e4bfc12434ed3e457
Leo Lee <alee@mdlive.com>
2022-06-08 14:28:04 -0400

    Fix rebase

--- lib/dock_health_api/crud/put.rb
+++ lib/dock_health_api/crud/put.rb
@@ -6,7 +6,7 @@ module DockHealthApi
         id = params.delete(:id)
         headers = {"Content-Type": "application/json", "x-api-key": "#{ENV["DOCK_HEALTH_API"]}", "x-user-id": "#{ENV["DOCK_USER"]}", "x-organization-id": "#{ENV["DOCK_ORG"]}"}
         response = execute_request(:put,
-                                   "#{resource_url}?#{id}",
+                                   "#{resource_url}/#{id}",
                                    headers: headers,
                                    body_params: params)
         return response.parsed if response.error
--- lib/dock_health_api/resources/tasklist.rb
+++ lib/dock_health_api/resources/tasklist.rb
@@ -6,11 +6,6 @@ module DockHealthApi
     extend DockHealthApi::Crud::Update
     extend DockHealthApi::Crud::List
 
-<<<<<<< HEAD
-=======
-    
-
->>>>>>> 593f283a8c1ec12c0d1382d8303b4c6ff636e5fb
     def self.resource_url
       "#{client.config.resource_url}/api/#{url_version}/list"
     end
@@ -21,13 +16,9 @@ module DockHealthApi
       extend DockHealthApi::Crud::Update
 
       def self.resource_url
-<<<<<<< HEAD
         "#{client.config.resource_url}/api/#{url_version}/list/user"
       end
 
-=======
-        "#{@@resource_url}/user"
->>>>>>> 593f283a8c1ec12c0d1382d8303b4c6ff636e5fb
       end
     end
   end
