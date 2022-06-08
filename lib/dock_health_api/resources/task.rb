85645552b3e11f8e1f942f349c17d5dd54dcc969
Leo Lee <alee@mdlive.com>
2022-06-08 09:29:59 -0400

    Add user under tasklist

--- lib/dock_health_api/resources/task.rb
+++ lib/dock_health_api/resources/task.rb
@@ -1,7 +1,7 @@
 module DockHealthApi
   class Task < Resource
     extend DockHealthApi::Crud::Create
-#    extend DockHealthApi::Crud::Delete  ## Waiting for delete to be ad in crud
+    extend DockHealthApi::Crud::Delete
     extend DockHealthApi::Crud::Get
     extend DockHealthApi::Crud::Update
     extend DockHealthApi::Crud::List
