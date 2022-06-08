cd5700742c0709eb57974afdeb49703464f61376
Leo Lee <alee@mdlive.com>
2022-06-08 09:29:59 -0400

    Add user under tasklist

--- lib/dock_health_api/resources/tasklist.rb
+++ lib/dock_health_api/resources/tasklist.rb
@@ -6,9 +6,20 @@ module DockHealthApi
     extend DockHealthApi::Crud::Update
     extend DockHealthApi::Crud::List
 
+    @@resource_url = "#{client.config.resource_url}/api/#{url_version}/list"
+
     def self.resource_url
-      "#{client.config.resource_url}/api/#{url_version}/list"
+      @@resource_url
     end
 
+    class User < TaskList
+      extend DockHealthApi::Crud::Create
+      extend DockHealthApi::Crud::Delete
+      extend DockHealthApi::Crud::Update
+
+      def self.resource_url
+        "#{@@resource_url}/user"
+      end
+    end
   end
 end
--- lib/dock_health_api/resources/webhook.rb
+++ lib/dock_health_api/resources/webhook.rb
@@ -1,7 +1,7 @@
 module DockHealthApi
   class Webhook < Resource
     extend DockHealthApi::Crud::Create
-#    extend DockHealthApi::Crud::Delete  ## Waiting for delete to be ad in crud
+    extend DockHealthApi::Crud::Delete
     extend DockHealthApi::Crud::Get
     extend DockHealthApi::Crud::Update
     extend DockHealthApi::Crud::List
@@ -9,6 +9,6 @@ module DockHealthApi
     def self.resource_url
       "#{client.config.resource_url}/api/#{url_version}/developer/#{class_name.downcase}"
     end
-    
+
   end
 end
