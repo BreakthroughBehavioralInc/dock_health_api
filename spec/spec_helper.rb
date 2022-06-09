c50a13523de5ce20204eb89a6de8e96f7ad6ed27
rmagomeromdlive <rmagomero@mdlive.com>
2022-06-09 10:30:55 -0400

    MLP-2309 create specs for Patient

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require "bundler/setup"
+require "dock_health_api"
+require "dotenv/load"
+require "pry"
+
+Dotenv.load
+
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  DockHealthApi.api_key = ENV["DOCK_HEALTH_KEY"]
+  DockHealthApi.api_secret = ENV["DOCK_HEALTH_SECRET"]
+  DockHealthApi.resource_url = ENV["DOCK_HEALTH_URL"]
+end
