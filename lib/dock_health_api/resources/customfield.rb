04b4283e09c9f998345a3a6bc06c87c7cf3101a5
Leo Lee <alee@mdlive.com>
2022-07-07 15:03:10 -0400

    Update list

--- lib/dock_health_api/resources/customfield.rb
+++ lib/dock_health_api/resources/customfield.rb
@@ -6,9 +6,15 @@ module DockHealthApi
     extend DockHealthApi::Crud::Update
     extend DockHealthApi::Crud::List
 
-
     def self.resource_url
-      "#{client.config.resource_url}/#{url_version}/configuration/field"
+      "#{client.config.resource_url}/api/#{url_version}/configuration/field"
+    end
+
+    def self.list(**params)
+      headers = {"x-api-key": "#{ENV["DOCK_HEALTH_API"]}", "x-user-id": "#{ENV["DOCK_USER"]}", "x-organization-id": "#{ENV["DOCK_ORG"]}"}
+      response = execute_request(:get, "#{resource_url}", headers: headers, params: params)
+      return response.parsed
+      new(response.parsed)
     end
   end
 end
