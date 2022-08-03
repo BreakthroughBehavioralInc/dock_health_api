d922a43069c41ef958a86021940f41e2a43ca008
Leo Lee <alee@mdlive.com>
2022-08-03 10:45:52 -0400

    Fix update

--- lib/dock_health_api/crud/update.rb
+++ lib/dock_health_api/crud/update.rb
@@ -2,21 +2,22 @@ module DockHealthApi
   module Crud
     module Update
       def update(**params)
+        require 'pry'
+        binding.pry
         headers = {"Content-Type": "application/json", "x-api-key": "#{ENV["DOCK_HEALTH_API"]}", "x-user-id": "#{ENV["DOCK_USER"]}", "x-organization-id": "#{ENV["DOCK_ORG"]}"}
-        # resource_url, params = add_organziation_user_fix(params) if params.key?(:organizationId) && params.key?(:userId)
+        if params.key?(:organizationId) && params.key?(:userId)
+          resource_url_fixed = "#{Organization.resource_url}/#{params[:organizationId]}/user"
+          params[:id] = params[:userId]
+        else
+          resource_url_fixed = resource_url
+        end
         id = params.delete(:id)
         response = execute_request(:patch,
-                                   "#{resource_url}/#{id}",
+                                   "#{resource_url_fixed}/#{id}",
                                    headers: headers,
                                    body_params: params)
         return response.parsed
         new(response.parsed)
-      end
-
-      def add_organziation_user_fix(params)
-        updated_resource_url = "#{Organization.resource_url}/#{params[:organizationId]}/user"
-        params[:id] = params[:userId]
-        return updated_resource_url, params
       end
     end
   end
