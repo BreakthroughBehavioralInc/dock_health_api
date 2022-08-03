d5909b5963e4fb4602416bafe12a8b382c8f951f
Leo Lee <alee@mdlive.com>
2022-08-03 11:13:36 -0400

    Update and add spec

--- lib/dock_health_api/crud/update.rb
+++ lib/dock_health_api/crud/update.rb
@@ -2,7 +2,6 @@ module DockHealthApi
   module Crud
     module Update
       def update(**params)
-        headers = {"Content-Type": "application/json", "x-api-key": "#{ENV["DOCK_HEALTH_API"]}", "x-user-id": "#{ENV["DOCK_USER"]}", "x-organization-id": "#{ENV["DOCK_ORG"]}"}
         if params.key?(:organizationId) && params.key?(:userId)
           resource_url_fixed = "#{Organization.resource_url}/#{params[:organizationId]}/user"
           params[:id] = params[:userId]
--- spec/task_group_spec.rb
+++ spec/task_group_spec.rb
@@ -3,9 +3,7 @@ require 'spec_helper'
 
 RSpec.describe DockHealthApi::Task::Group do
   let(:tasklistid) { DockHealthApi::TaskList.list.last["id"] }
-  # tasklistid = DockHealthApi::TaskList.list.last["id"]
   let(:params) {{ groupName: "test foobar", taskList: { type: "DEVELOPER", id: tasklistid } }}
-  # params = { groupName: "test foobar", taskList: { type: "DEVELOPER", id: tasklistid } }
   id = ""
 
   describe "#create" do
@@ -31,12 +29,12 @@ RSpec.describe DockHealthApi::Task::Group do
   end
 
   describe "#get" do
-    # context "get a specific task group" do
-    #   it "should get the correct task group" do
-    #     response = DockHealthApi::Task::Group.get(id)
-    #     expect(response["groupName"]).to eq(params[:groupName])
-    #   end
-    # end
+    context "get a specific task group" do
+      it "should get the correct task group" do
+        response = DockHealthApi::Task::Group.get(id)
+        expect(response["groupName"]).to eq(params[:groupName])
+      end
+    end
 
     context "get task group with wrong id" do
       it "should return 404" do
@@ -47,26 +45,26 @@ RSpec.describe DockHealthApi::Task::Group do
     end
   end
 
-  # describe "#update" do
-  #   context "update existing task group" do
-  #     it "should update existing task group" do
-  #       new_params = { grupName: "test foobar update", id: id }
-  #       response = DockHealthApi::Task::Group.update(new_params)
-  #       expect(response["groupName"]).to eq(new_params[:groupName])
-  #     end
-  #   end
-  # end
+  describe "#update" do
+    context "update existing task group" do
+      it "should update existing task group" do
+        new_params = { grupName: "test foobar update", id: id }
+        response = DockHealthApi::Task::Group.update(new_params)
+        expect(response["groupName"]).to eq(new_params[:groupName])
+      end
+    end
+  end
 
-  # describe "#delete" do
-  #   context "Delete existing task group" do
-  #     it "should delete existing task group" do
-  #       initial_count = DockHealthApi::Task::Group.list(taskListIdentifier: tasklistid).count
-  #       response = DockHealthApi::Task::Group.delete(id: id)
-  #       final_count = DockHealthApi::Task::Group.list(taskListIdentifier: tasklistid).count
-  #       expect(response).to eq("")
-  #       expect(final_count - initial_count).to eq(-1)
-  #     end
-  #   end
-  # end
+  describe "#delete" do
+    context "Delete existing task group" do
+      it "should delete existing task group" do
+        initial_count = DockHealthApi::Task::Group.list(taskListIdentifier: tasklistid).count
+        response = DockHealthApi::Task::Group.delete(id: id)
+        final_count = DockHealthApi::Task::Group.list(taskListIdentifier: tasklistid).count
+        expect(response["groupName"]).to eq(params[:groupName])
+        expect(final_count - initial_count).to eq(-1)
+      end
+    end
+  end
 
 end
