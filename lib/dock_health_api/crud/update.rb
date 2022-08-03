4d6c1197a8c07b67bfc1933891e6b383c5722c56
Leo Lee <alee@mdlive.com>
2022-08-03 12:54:13 -0400

    overwrite update in organization user

--- lib/dock_health_api/crud/update.rb
+++ lib/dock_health_api/crud/update.rb
@@ -2,15 +2,9 @@ module DockHealthApi
   module Crud
     module Update
       def update(**params)
-        if params.key?(:organizationId) && params.key?(:userId)
-          resource_url_fixed = "#{Organization.resource_url}/#{params[:organizationId]}/user"
-          params[:id] = params[:userId]
-        else
-          resource_url_fixed = resource_url
-        end
         id = params.delete(:id)
         response = execute_request(:patch,
-                                   "#{resource_url_fixed}/#{id}",
+                                   "#{resource_url}/#{id}",
                                    headers: headers,
                                    body_params: params)
         return response.parsed
--- lib/dock_health_api/resources/organization.rb
+++ lib/dock_health_api/resources/organization.rb
@@ -10,6 +10,18 @@ module DockHealthApi
       def self.resource_url
         "#{Organization.resource_url}/#{DockHealthApi.org_id}/user"
       end
+
+      def self.update(**params)
+        resource_url_fixed = "#{Organization.resource_url}/#{params[:organizationId]}/user"
+        params[:id] = params[:userId]
+        id = params.delete(:id)
+        response = execute_request(:patch,
+                                   "#{resource_url_fixed}/#{id}",
+                                   headers: headers,
+                                   body_params: params)
+        return response.parsed
+        new(response.parsed)
+      end
     end
   end
 end
